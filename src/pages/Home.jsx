e90a17f1997f6b3c2a7bb28b005a911a12d6a671
Jxpo <tomasdomingues51@gmail.com>
2025-02-12 11:28:47 -0300

    homeatt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,25 @@ import "../../src/styles/home.css";
 function Home() {
   return (
     <div className="home-container">
-      <div className="img-home"></div>
-      dfsfdfsfsfsdfsdf
-      <div></div>
+      <div className="fodacitron">
+        <div className="img-wrapper">
+          <div className="img-home"></div>
+          <div className="text-overlay">
+            <p>Bem vindo ao Lessie</p>
+            <p className="Lessie-logo">LESSIE</p>
+            <p className="putaqpariu">“ Mergulhar no mundo da leitura, permite-nos viajar sem tirar os pés do chão “
+            - Iolanda Brazão</p>
+          </div>
+        </div>
+      </div>
+      <div className="segundaparte-home">
+        <div className="recomendacoes">
+          <h2>RECOMENDAÇÕES DO DIA</h2>
+        </div>
+        <div className="livros-recomendados">
+          
+        </div>
+      </div>
     </div>
   );
 }
