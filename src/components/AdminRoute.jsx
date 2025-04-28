b73ce3255ca263047dc3c5c159f027da8b2a2857
oliveeira2610 <bielolirodrigues@gmail.com>
2025-04-28 13:54:23 -0300

    .

--- src/components/AdminRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const AdminRoute = ({ children }) => {
-  const userRole = localStorage.getItem("userRole");
-
-  return userRole === "admin" ? children : <Navigate to="/home" />;
-};
-
-export default AdminRoute;
